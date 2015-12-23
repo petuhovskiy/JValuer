0e5d9722684deeba50c96289dc3f69c10d4a22f6
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2015-12-23 10:24:43 +0300

    osx invoker fixed

---
+++ src/com/petukhovsky/jvaluer/OSXInvoker.java
@@ -0,0 +1,68 @@
+package com.petukhovsky.jvaluer;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Created by qjex on 12/21/15.
+ */
+
+public class OSXInvoker implements Invoker {
+
+    private static Path runexe = Local.getOSXRunExe();
+    private static DocumentBuilderFactory factory;
+    private static DocumentBuilder builder;
+
+    static {
+        factory = DocumentBuilderFactory.newInstance();
+        try {
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public RunInfo run(RunOptions options) {
+        RunInfo info = new RunInfo();
+
+        try {
+            String cmd = String.format("%s -xml", runexe);
+
+            if (options.hasParameter("folder")) cmd += " -d \"" + options.getParameter("folder") + "\"";
+            if (options.hasParameter("stdin")) cmd += " -i \"" + options.getParameter("stdin") + "\"";
+            if (options.hasParameter("stdout")) cmd += " -o \"" + options.getParameter("stdout") + "\"";
+            if (options.hasParameter("login")) cmd += " -l " + options.getParameter("login");
+            if (options.hasParameter("password")) cmd += " -p " + options.getParameter("password");
+            if (options.hasParameter("memory_limit")) cmd += " -m " + options.getParameter("memory_limit");
+            if (options.hasParameter("time_limit")) cmd += " -t " + options.getParameter("time_limit");
+            cmd += " \"" + options.getParameter("executable") + "\"";
+
+            if (Local.isDebug()) System.out.println("OSXInvoker runs runexe with cmd: " + cmd);
+
+            Process process = Runtime.getRuntime().exec(new String[]{"bash", "-c", cmd});
+            process.waitFor();
+
+            Document doc = builder.parse(process.getInputStream());
+            String verdict = doc.getElementsByTagName("invocationVerdict").item(0).getTextContent();
+            int exitCode = Integer.parseInt(doc.getElementsByTagName("exitCode").item(0).getTextContent());
+            int userTime = Integer.parseInt(doc.getElementsByTagName("processorUserModeTime").item(0).getTextContent());
+            int kernelTime = Integer.parseInt(doc.getElementsByTagName("processorKernelModeTime").item(0).getTextContent());
+            int passedTime = Integer.parseInt(doc.getElementsByTagName("passedTime").item(0).getTextContent());
+            int consumedMemory = Integer.parseInt(doc.getElementsByTagName("consumedMemory").item(0).getTextContent());
+            String comment = doc.getElementsByTagName("comment").item(0).getTextContent();
+
+            info.completed(RunVerdict.valueOf(verdict), exitCode, userTime, kernelTime, passedTime, consumedMemory, comment);
+        } catch (IOException | InterruptedException | SAXException e) {
+            info.crashed("Crashed while invoking");
+        }
+
+        return info;
+    }
+}
