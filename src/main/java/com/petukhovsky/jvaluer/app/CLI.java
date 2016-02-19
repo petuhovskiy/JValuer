1c07386abc0564363e6c9c449ec06224c2d2b97f
petuhovskiy <petuhovskiy@yandex.ru>
2016-02-20 00:00:00 +0300

    Begin of cli implementation

---
+++ src/main/java/com/petukhovsky/jvaluer/app/CLI.java
@@ -0,0 +1,33 @@
+package com.petukhovsky.jvaluer.app;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by petuh on 2/19/2016.
+ */
+public class CLI {
+
+    private Scanner sc;
+    private PrintStream pw;
+
+    private CLI() {
+        sc = new Scanner(System.in);
+        pw = System.out;
+    }
+
+    public static void main(String[] args) {
+        new CLI().start();
+    }
+
+    private void start() {
+        while (true) {
+            pw.print("jvaluer>");
+            String command = sc.nextLine().trim();
+            if (command.isEmpty()) continue;
+            String arr[] = command.split("\\s+");
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
